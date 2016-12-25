4f4ec3c59c0a9fdc4bec49aa5b28df85430f4638
Lukas Zhao <Lukas Zhao>
2016-12-24 23:38:07 -0500

    reading data file and plotting data points ok

---
+++ Data_Fit/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Csharp_001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonPlotData = new System.Windows.Forms.Button();
+            this.buttonReadData = new System.Windows.Forms.Button();
+            this.richTextBox_msg = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(32, 36);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "Data";
+            series1.YValuesPerPoint = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Fit";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(425, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // buttonPlotData
+            // 
+            this.buttonPlotData.Location = new System.Drawing.Point(515, 65);
+            this.buttonPlotData.Name = "buttonPlotData";
+            this.buttonPlotData.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlotData.TabIndex = 1;
+            this.buttonPlotData.Text = "Plot data";
+            this.buttonPlotData.UseVisualStyleBackColor = true;
+            this.buttonPlotData.Click += new System.EventHandler(this.buttonPlotData_Click);
+            // 
+            // buttonReadData
+            // 
+            this.buttonReadData.Location = new System.Drawing.Point(515, 36);
+            this.buttonReadData.Name = "buttonReadData";
+            this.buttonReadData.Size = new System.Drawing.Size(75, 23);
+            this.buttonReadData.TabIndex = 2;
+            this.buttonReadData.Text = "Read Data";
+            this.buttonReadData.UseVisualStyleBackColor = true;
+            this.buttonReadData.Click += new System.EventHandler(this.buttonReadData_Click);
+            // 
+            // richTextBox_msg
+            // 
+            this.richTextBox_msg.Location = new System.Drawing.Point(515, 223);
+            this.richTextBox_msg.Name = "richTextBox_msg";
+            this.richTextBox_msg.Size = new System.Drawing.Size(308, 182);
+            this.richTextBox_msg.TabIndex = 3;
+            this.richTextBox_msg.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 460);
+            this.Controls.Add(this.richTextBox_msg);
+            this.Controls.Add(this.buttonReadData);
+            this.Controls.Add(this.buttonPlotData);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Data Fit";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button buttonPlotData;
+        private System.Windows.Forms.Button buttonReadData;
+        private System.Windows.Forms.RichTextBox richTextBox_msg;
+    }
+}
+
---
+++ Data_Fit/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;   //for Debug.WriteLine()
+using System.IO;
+
+namespace Csharp_001
+{
+    public partial class Form1 : Form
+    {
+        private List<double> data_x = new List<double>();    //note: in C#, a List is like a vector in C++; C#'s LinkedList is like list in C++
+        private List<double> data_y = new List<double>();
+        private List<double> fit_x = new List<double>();
+        private List<double> fit_y = new List<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            assignDummyData();
+        }
+
+        private void assignDummyData()
+        {
+            for(int i=1; i<101; ++i)
+            {
+                double x = Convert.ToDouble(i) / 10.0;
+                double y = Math.Exp(-Math.Pow((x - 5),2));
+                chart1.Series["Data"].Points.AddXY(x, y);
+                chart1.Series["Fit"].Points.AddXY(x, y);
+            }
+        }
+
+        private void buttonPlotData_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonReadData_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(File.OpenRead(ofd.FileName));
+                data_x.Clear();
+                data_y.Clear();
+                fit_x.Clear();
+                fit_y.Clear();
+                while(sr.EndOfStream == false)
+                {
+                    string line = sr.ReadLine();
+                    if (line == "") { continue; }
+                    string[] parsedWords = stringParser(line);
+                    if (parsedWords[0] != "failed")
+                    {
+                        data_x.Add(Convert.ToDouble(parsedWords[0]));
+                        data_y.Add(Convert.ToDouble(parsedWords[1]));
+                    }
+                    
+                }
+                plotData();
+                sr.Dispose();
+            }
+        }
+
+        public string[] stringParser(string line)
+        {
+            char[] delimiterChars = { ' ', ',', ':', '\t' };
+            string[] words = line.Split(delimiterChars);
+            string[] result = new string[2];
+            int j = 0;
+            for(int i = 0; i < words.Length; ++i)
+            {
+                if (words[i] != "")
+                {
+                    result[j] = words[i];
+                    ++j;  
+                }
+                if (j == 2) { break; }
+            }
+            if (j != 2) { result[0] = "failed"; result[1] = "failed"; }
+            return result;
+        }
+
+        private void plotData()
+        {
+            chart1.Series["Data"].Points.Clear();
+            chart1.Series["Fit"].Points.Clear();
+            for(int i=0; i < data_x.Count; ++i)
+            {
+                chart1.Series["Data"].Points.AddXY(data_x[i], data_y[i]);
+            }
+        }
+    }
+}
